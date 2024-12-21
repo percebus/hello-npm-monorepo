436b6abc1b00ba8b160110f153b5eb3c2b246173
JC Guerrero <JCystems@gMail.com>
2024-12-20 19:19:46 -0600

    Update task.ba.sh

--- scripts/node/packages/task.ba.sh
+++ scripts/node/packages/task.ba.sh
@@ -1,18 +1,18 @@
 # !/bin/bash
 
 set -e
-set -v
 
 cmd=${1}
-
 if [ -z ${cmd} ]; then
   cmd="test"
 fi
+
+set -x
 
 # TODO for each package
 npm run sub:express-app:${cmd}
 npm run sub:rest-app:${cmd}
 npm run sub:package-c:${cmd}
 
-set +v
+set +x
 set +e
